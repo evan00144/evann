96349c551a1c6bdb62cb4879527f2e8643f83749
evan00144 <evan2@student.umn.ac.id>
2023-08-30 11:06:32 +0700

    add analytics vercel

--- src/App.tsx
+++ src/App.tsx
@@ -6,36 +6,48 @@ import Header from "./pages/Header/Header";
 import SectionOne3D from "./pages/Section/SectionOne3D";
 import SectionFourTestimonial from "./pages/Section/SectionFourTestimonial";
 import SectionTwoPage from "./pages/Section/SectionTwo/SectionTwoPage";
-import { CssBaseline, PaletteMode, StyledEngineProvider, createTheme } from "@mui/material";
+import {
+  CssBaseline,
+  PaletteMode,
+  StyledEngineProvider,
+  createTheme,
+} from "@mui/material";
 import { useMemo } from "react";
 import { getDesignTokens } from "./theme";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { toggleThemeMode } from "./store/reducer/ui";
 import SectionThreePageTwo from "./pages/Section/SectionThree/SectionThreePageTwo";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   // const [mode, setMode] = useState<"light" | "dark">("dark");
-  const {themeMode}  = useAppSelector(state => state.ui)
-  const dispatch = useAppDispatch()
+  const { themeMode } = useAppSelector((state) => state.ui);
+  const dispatch = useAppDispatch();
   const toggleColorMode = () => {
-    dispatch(toggleThemeMode())
+    dispatch(toggleThemeMode());
   };
 
-  const theme = useMemo(() => createTheme(getDesignTokens(themeMode as PaletteMode)), [themeMode]);
+  const theme = useMemo(
+    () => createTheme(getDesignTokens(themeMode as PaletteMode)),
+    [themeMode]
+  );
 
   return (
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <StyledProvider theme={theme}>
-          <CssBaseline />
-          <Header toggleColorMode={toggleColorMode} />
-          <SectionOne3D />
-          <SectionTwoPage />
-          <SectionThreePageTwo/>
-          <SectionFourTestimonial />
-        </StyledProvider>
-      </ThemeProvider>
-    </StyledEngineProvider>
+    <>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <StyledProvider theme={theme}>
+            <CssBaseline />
+            <Header toggleColorMode={toggleColorMode} />
+            <SectionOne3D />
+            <SectionTwoPage />
+            <SectionThreePageTwo />
+            <SectionFourTestimonial />
+          </StyledProvider>
+        </ThemeProvider>
+      </StyledEngineProvider>
+      <Analytics />
+    </>
   );
 }
 
